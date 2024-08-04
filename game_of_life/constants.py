cef7b03354b9ed49c69c8f09492781b1a92548b1
mmarchi <mattia.marchi@hotmail.it>
2024-08-04 14:56:23 +0200

    Added sprites

---
+++ game_of_life/constants.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+PARENT_DIR = Path(__file__).parent
+STATIC_DIR = PARENT_DIR / 'static'
+SPRITE_DIR = STATIC_DIR / 'sprites'
+HUMAN_SPRITE_DIR = SPRITE_DIR / 'humans'
+
+# Human sprites
+GENERIC_MALE_SPRITE = str(HUMAN_SPRITE_DIR / 'generic_male.png')
+GENERIC_FEMALE_SPRITE = str(HUMAN_SPRITE_DIR / 'generic_female.png')
+
+KNIGHT_SPRITE = str(HUMAN_SPRITE_DIR / 'knight.png')
+WIZARD_SPRITE = str(HUMAN_SPRITE_DIR / 'wizard.png')
+BLACKSMITH_SPRITE = str(HUMAN_SPRITE_DIR / 'blacksmith.png')
+FARMER_SPRITE = str(HUMAN_SPRITE_DIR / 'farmer.png')
